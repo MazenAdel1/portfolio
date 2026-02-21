7bc5e61fe81ea90e67040b56b37124539f2bafcd
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-21 23:04:35 +0200

    increase delay

--- src/components/sections/landing/Landing.tsx
+++ src/components/sections/landing/Landing.tsx
@@ -18,26 +18,29 @@ export default function Landing() {
   const bottomY = useTransform(scrollYProgress, [0, 0.3], ["0%", "-60%"]);
 
   return (
-    <main className="corner-squircle h-[calc(100dvh-4rem)] overflow-hidden rounded-[50px] bg-[url('/landing/home-background.jpg')] bg-cover bg-center">
+    <section
+      id="landing"
+      className="section overflow-hidden bg-[url('/landing/home-background.webp')]"
+    >
       <motion.div
         style={{ y: textY, filter: textBlur, opacity: textOpacity }}
-        className="font-telma absolute top-1/2 left-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center"
+        className="font-telma absolute top-1/2 left-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center select-none"
       >
         <AnimatedText
           text="Mazen Adel"
           as={"h1"}
           staggerChildren={0.33}
           duration={0.33}
-          delay={0.67}
-          className="text-[150px] font-black uppercase text-shadow-(--drop-shadow-solid)"
+          delay={1.67}
+          className="text-[11dvw] font-black uppercase text-shadow-(--drop-shadow-solid)"
         />
         <AnimatedText
           text="Front-End Developer"
           as={"h2"}
           staggerChildren={0.33}
           duration={0.33}
-          delay={1.33}
-          className="-mt-12 text-6xl font-semibold capitalize"
+          delay={2.33}
+          className="-mt-[4dvw] text-[5dvw] font-semibold capitalize"
         />
       </motion.div>
 
@@ -48,23 +51,23 @@ export default function Landing() {
         <motion.div
           initial={{ opacity: 0, y: 10, filter: "blur(5px)" }}
           animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-          transition={{ delay: 2, duration: 0.33, ease: "easeOut" }}
+          transition={{ delay: 3, duration: 0.33, ease: "easeOut" }}
         >
           <Image
-            src="/arrow-down.svg"
-            alt="vector"
+            src="/icons/arrow-down.svg"
+            alt="arrow down icon"
             width={100}
             height={100}
-            className="translate-x-12 translate-y-5"
+            className="w-20 translate-x-12 translate-y-5 md:w-25"
           />
           <Link
             href="/#projects"
-            className="block -translate-y-6 stroke-[0.5px] text-xl text-shadow-lg"
+            className="block -translate-y-6 stroke-[0.5px] text-lg text-shadow-lg md:text-xl"
           >
             See Projects
           </Link>
         </motion.div>
       </motion.div>
-    </main>
+    </section>
   );
 }
