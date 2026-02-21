72e7c55038b4240c2bf1529fec84a63c0ce6ee07
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-21 23:10:47 +0200

    remove loading overlay

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,2 @@
 export { default as AnimatedText } from "./AnimatedText";
-export { default as LoadingOverlay } from "./LoadingOverlay";
 export { default as AnimationWrapper } from "./AnimationWrapper";
