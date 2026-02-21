44ddb7c3d142c4219f87341446176381786ffc39
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-21 22:57:38 +0200

    fire animation while in view

--- src/components/sections/contact/ContactSection.tsx
+++ src/components/sections/contact/ContactSection.tsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import { ContactSectionProps } from "./types";
+import * as motion from "motion/react-client";
 
 export default function ContactSection({ title, links }: ContactSectionProps) {
   return (
-    <div className="flex w-md flex-col gap-3">
-      <h4 className="border-b-2 border-gray-200 text-2xl font-semibold">
+    <div className="flex w-full flex-col gap-3 md:w-md">
+      <h3 className="border-b-2 border-gray-200 text-xl font-semibold md:text-2xl">
         {title}
-      </h4>
+      </h3>
       {links.map((link) => (
-        <Link
+        <motion.div
+          initial={{ opacity: 0, filter: "blur(5px)" }}
+          whileInView={{ opacity: 1, filter: "blur(0px)" }}
+          transition={{ duration: 0.33, delay: 0.2 }}
+          viewport={{ once: true }}
           key={link.href}
-          href={link.href}
-          target="_blank"
-          className="w-fit text-xl font-medium transition hover:text-yellow-300"
         >
-          {link.name}
-        </Link>
+          <Link
+            key={link.href}
+            href={link.href}
+            target="_blank"
+            className="w-fit text-lg font-medium transition hover:text-yellow-300 md:text-xl"
+          >
+            {link.name}
+          </Link>
+        </motion.div>
       ))}
     </div>
   );
