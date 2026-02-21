9549115d471185e3006e440f990a6c6b52fa6bdf
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-21 23:03:28 +0200

    change styles

--- src/components/sections/experience/Experience.tsx
+++ src/components/sections/experience/Experience.tsx
@@ -3,7 +3,10 @@ import ExperienceItem from "./ExperienceItem";
 
 export default function Experience() {
   return (
-    <section id="experience" className="flex flex-col text-black">
+    <section
+      id="experience"
+      className="flex flex-col gap-10 text-black md:gap-0"
+    >
       {experiences.map((exp, i) => (
         <ExperienceItem key={i} {...exp} />
       ))}
