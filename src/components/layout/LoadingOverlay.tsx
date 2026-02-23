0af6a1b0cdec71ad87b06829f298bbfbcd8b32c2
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-23 21:41:04 +0200

    make overlay bigger to fix animation issue on mobile screens and make it faster

--- src/components/layout/LoadingOverlay.tsx
+++ src/components/layout/LoadingOverlay.tsx
@@ -2,7 +2,7 @@ export default function LoadingOverlay() {
   return (
     <section
       id="loading"
-      className="corner-squircle outline-background fixed top-1/2 left-1/2 z-100 flex h-0 w-0 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-transparent outline-[100dvw]"
+      className="corner-squircle outline-background fixed top-1/2 left-1/2 z-100 flex h-0 w-0 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-transparent outline-[150dvw]"
     />
   );
 }
