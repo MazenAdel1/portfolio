10770a5ac06ac3247187ac83cd88fa75034571da
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-23 22:15:14 +0200

    wrap header in ScrollSectionsProvider

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,15 @@
 import NavLinks from "./NavLinks";
 import MobileMenu from "./MobileMenu";
+import { ScrollSectionsProvider } from "@/hooks/useScrollSections";
 
 export default function Header() {
   return (
-    <header className="glass corner-squircle fixed top-7 left-1/2 z-50 -translate-x-1/2 rounded-3xl px-6 py-4 md:top-10">
-      <NavLinks />
+    <ScrollSectionsProvider>
+      <header className="glass corner-squircle fixed top-7 left-1/2 z-50 -translate-x-1/2 rounded-3xl px-6 py-4 md:top-10">
+        <NavLinks />
 
-      <MobileMenu />
-    </header>
+        <MobileMenu />
+      </header>
+    </ScrollSectionsProvider>
   );
 }
