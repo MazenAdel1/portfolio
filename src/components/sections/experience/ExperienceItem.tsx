33c499a5346c6631f4669297941ec05e5a366db8
MazenAdel1 <mazenadel.dev@gmail.com>
2026-02-23 22:10:52 +0200

    change image sizes

--- src/components/sections/experience/ExperienceItem.tsx
+++ src/components/sections/experience/ExperienceItem.tsx
@@ -30,8 +30,9 @@ export default function ExperienceItem({
         <Image
           src={image}
           alt={alt}
-          width={1000}
-          height={1000}
+          width={1024}
+          height={576}
+          sizes="(max-width: 768px) 100vw, 50vw"
           className="w-full"
         />
       </div>
